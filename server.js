4627d4184c86dcab3c379a32b58028c0ddb8a441
rvision0925 <v7sion@topdogtek.com>
2021-08-01 20:19:53 -0400

    setup db user schema

--- server.js
+++ server.js
@@ -4,23 +4,14 @@ const bodyParser = require("body-parser");
 const app = express();
 
 // Bodyparser middleware
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// DB Config
+// DB
 const db = require("./config/keys").mongoURI;
-// Connect to MongoDB
-mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log("MongoDB connection error: ", err));
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("\nMongoDB successfully connected"))
+  .catch(err => console.log("\nMongoDB connection error: ", err));
   
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
